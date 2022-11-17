268a7c99612eef6ca6946def1b988f9c7531a8dc
Edilson Gonçalves Oliveira <101567222+EdilsonXX@users.noreply.github.com>
2022-11-17 07:53:36 -0300

    Add files via upload

---
+++ Criterio2/src/main/java/com/example/demo/Criterio2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Criterio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Criterio2Application.class, args);
+	}
+
+}
